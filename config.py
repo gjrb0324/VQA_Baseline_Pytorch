78f0d5c1a4a15c5dc67d6d3a254dc74b6140cdcc
Gyu Huh <kyuh1882@gmail.com>
2022-01-24 13:53:26 +0900

    log file root typo

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ output_features = 2048  # number of feature maps thereof
 central_fraction = 0.875  # only take this much of the centre when scaling and centre cropping
 
 # training config
-epochs = 50
+epochs = 2
 max_answers = 3000
 batch_size = 128
 initial_lr = 1e-3  # default Adam lr
--- train.py
+++ train.py
@@ -103,7 +103,7 @@ def main():
     else:
         from datetime import datetime
         name = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    target_name = os.path.join('logs', '{}.pth'.format(name))
+    target_name = os.path.join('C:\\temp_vqa\\pytorch-vqa\\VQA_Baseline_Pytorch\\logs', '{}.pth'.format(name))
     print('will save to {}'.format(target_name))
 
     cudnn.benchmark = True
